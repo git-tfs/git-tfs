24c606762ef6d89b4e4b4b8eee2e14e31d0220ec
Matt Burke <maburke@sep.com>
2009-12-03 16:55:05 -0500

    Added a stub of a checkin command.

--- GitTfs/Commands/Checkin.cs
+++ GitTfs/Commands/Checkin.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using CommandLine.OptParse;
+
 namespace Sep.Git.Tfs.Commands
 {
-    [Pluggable("checkin")]
     public class Checkin : GitTfsCommand
     {
+        /*
         [OptDef(OptValType.Flag)]
         [ShortOptionName('m')]
         [ShortOptionName('M')]
@@ -40,5 +43,30 @@ namespace Sep.Git.Tfs.Commands
         private CommitOptions commitOptions;
         private FcOptions fcOptions;
         private RemoteOptions remoteOptions;
+        */
+
+
+        public IEnumerable<IOptionResults> ExtraOptions
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Run(IList<string> args)
+        {
+            throw new System.NotImplementedException();
+            // Outline of git svn dcommit (rev 4f2b15ce88b70dd9e269517a9903864393ca873b):
+            // 463:     Ensure index is clean.
+            // 466:     assume HEAD is what we want to send to SVN, if no head is specified. Set $head to the thing to dcommit.
+            // 470-477: set $old_head to current head so we can put it back later.
+            // 478:     checkout $head.
+            // 482:     Get SVN parent commit, and keep the list of local commits in between.
+            // 488-496: Get SVN url from command line or config or fall back to commit info.
+            // ...      clone commit in svn rev, rebase (make the git commit go away ?)
+            // 607:     Restore $old_head.
+            //
+            // linearize_history:
+            // 1455-1457: Take a list of commit refs, and make a hash of commit => [parents] in %parents.
+            // profit?
+        }
     }
 }
