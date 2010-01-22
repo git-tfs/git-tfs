23309c5fc8a340228b43f46bec51c6692c991dcc
Matt Burke <maburke@sep.com>
2010-01-22 15:44:54 -0500

    Added a command to dump configuration information.

---
+++ GitTfs/Commands/Diagnostics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine.OptParse;
+using StructureMap;
+
+namespace Sep.Git.Tfs.Commands
+{
+    [Pluggable("diagnostics")]
+    public class Diagnostics : GitTfsCommand
+    {
+        private readonly TextWriter _stdout;
+
+        public Diagnostics(TextWriter stdout)
+        {
+            _stdout = stdout;
+        }
+
+        public IEnumerable<IOptionResults> ExtraOptions
+        {
+            get { return this.MakeOptionResults(); }
+        }
+
+        public int Run(IList<string> args)
+        {
+            _stdout.WriteLine(ObjectFactory.WhatDoIHave());
+            return GitTfsExitCodes.OK;
+        }
+    }
+}
