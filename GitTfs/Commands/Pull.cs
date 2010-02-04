3bed67f5006155d05b0555e1d33e01185982793b
Robert Uhl <rcuhl@sep.com>
2010-02-04 16:42:58 -0500

    Last few changes from help fix.

--- GitTfs/Commands/Pull.cs
+++ GitTfs/Commands/Pull.cs
@@ -8,9 +8,6 @@ using StructureMap;
 
 namespace Sep.Git.Tfs.Commands
 {
-    //#todo
-    //Figure out how to get fetches options to show up.
-    //http://git.kernel.org/?p=git/git.git;a=blob;f=git-pull.sh;h=38331a861106c63bf5f421dbe03f4aafe949812e;hb=HEAD git-pull's shell script
     [Pluggable("pull")]
     [Description("pull [options] tfs-url repository-path")]
     [RequiresValidGitRepository]
