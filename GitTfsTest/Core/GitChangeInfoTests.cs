7acceda08172a2169a8b041c413a2febdcfc3e23
Matt Burke <maburke@sep.com>
2010-10-13 10:02:45 -0400

    Added some tests for GitChangeInfo.

---
+++ GitTfsTest/Core/GitChangeInfoTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sep.Git.Tfs.Core;
+using GitSharp.Core;
+
+namespace Sep.Git.Tfs.Test.Core
+{
+    [TestClass]
+    public class GitChangeInfoTests
+    {
+        [TestMethod]
+        public void GetsMode()
+        {
+            var line = ":000000 100644 abcdef0123abcdef0123abcdef0123abcdef0123 01234567ab01234567ab01234567ab01234567ab M\tblah";
+            var info = GitChangeInfo.Parse(line);
+            Assert.AreEqual("100644", info.NewMode.ToModeString());
+        }
+
+        [TestMethod]
+        public void GetsLinkMode()
+        {
+            var line = ":000000 160000 abcdef0123abcdef0123abcdef0123abcdef0123 01234567ab01234567ab01234567ab01234567ab M\tblah";
+            var info = GitChangeInfo.Parse(line);
+            Assert.AreEqual(FileMode.GitLink, info.NewMode);
+        }
+
+        [TestMethod]
+        public void GetsChangeType()
+        {
+            var line = ":000000 100644 abcdef0123abcdef0123abcdef0123abcdef0123 01234567ab01234567ab01234567ab01234567ab M\tblah";
+            var info = GitChangeInfo.Parse(line);
+            Assert.AreEqual("M", info.Status);
+        }
+
+        [TestMethod]
+        public void GetsChangeTypeWhenScoreIsPresent()
+        {
+            var line = ":000000 100644 abcdef0123abcdef0123abcdef0123abcdef0123 01234567ab01234567ab01234567ab01234567ab R001\tblah\tnewblah";
+            var info = GitChangeInfo.Parse(line);
+            Assert.AreEqual("R", info.Status);
+        }
+    }
+}
