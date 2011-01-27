f8bd673840b882a1cfc668af007f5482e1598058
Matt Burke <maburke@sep.com>
2011-01-27 15:22:17 -0500

    Added a merge-back option (pull) to checkin.

--- GitTfs/Commands/Checkin.cs
+++ GitTfs/Commands/Checkin.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
 using CommandLine.OptParse;
 using Sep.Git.Tfs.Core;
 using StructureMap;
@@ -18,6 +16,12 @@ namespace Sep.Git.Tfs.Commands
         private readonly CheckinOptions _checkinOptions;
         private readonly TfsWriter _writer;
 
+        [OptDef(OptValType.Flag)]
+        [LongOptionName("pull")]
+        [UseNameAsLongOption(false)]
+        [Description("Merges the new TFS changeset to this branch, if checking in HEAD.")]
+        public bool MergeBack { get; set; }
+
         public Checkin(TextWriter stdout, CheckinOptions checkinOptions, TfsWriter writer)
         {
             _writer = writer;
@@ -27,22 +31,26 @@ namespace Sep.Git.Tfs.Commands
 
         public IEnumerable<IOptionResults> ExtraOptions
         {
-            get { return this.MakeOptionResults(_checkinOptions); }
+            get { return this.MakeNestedOptionResults(_checkinOptions); }
         }
 
-        // TODO: DRY up, w.r.t. with Shelve.
         public int Run()
         {
+            MergeBack = true;
             return Run("HEAD");
         }
 
         public int Run(string refToCheckin)
         {
+            if(refToCheckin != "HEAD" && MergeBack)
+                throw new GitTfsException("Can't pull unless checking in HEAD!");
             return _writer.Write(refToCheckin, changeset =>
             {
                 var newChangeset = changeset.Remote.Checkin(refToCheckin, changeset);
                 _stdout.WriteLine("TFS Changeset #" + newChangeset + " was created. Marking it as a merge commit...");
                 changeset.Remote.Fetch(new Dictionary<long, string> { { newChangeset, refToCheckin } });
+                if(MergeBack)
+                    changeset.Remote.Repository.CommandNoisy("merge", changeset.Remote.MaxCommitHash);
                 return GitTfsExitCodes.OK;
             });
         }
