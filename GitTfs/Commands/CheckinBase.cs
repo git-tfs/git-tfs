7f9784ab72701b07d3cda77325e678f9f766dfa5
ivan-danilov <van.danilov@gmail.com>
2011-06-21 05:54:12 +0300

    Added --rebase-workflow option to git tfs checkin command

--- GitTfs/Commands/CheckinBase.cs
+++ GitTfs/Commands/CheckinBase.cs
@@ -30,15 +30,41 @@ namespace Sep.Git.Tfs.Commands
 
         public int Run(string refToCheckin)
         {
-            return _writer.Write(refToCheckin, changeset =>
-                                                   {
-                                                       var newChangesetId = DoCheckin(changeset, refToCheckin);
-                                                       _stdout.WriteLine("TFS Changeset #" + newChangesetId + " was created. Marking it as a merge commit...");
-                                                       changeset.Remote.Fetch(new Dictionary<long, string> { { newChangesetId, refToCheckin } });
-                                                       if (refToCheckin == "HEAD")
-                                                           changeset.Remote.Repository.CommandNoisy("merge", changeset.Remote.MaxCommitHash);
-                                                       return GitTfsExitCodes.OK;
-                                                   });
+            return _writer.Write(refToCheckin, changeset => PerformCheckin(changeset, refToCheckin));
+        }
+
+        private int PerformCheckin(TfsChangesetInfo parentChangeset, string refToCheckin)
+        {
+            if (_checkinOptions.RebaseWorkflow)
+            {
+                var newChangesetId = DoCheckin(parentChangeset, refToCheckin);
+                _stdout.WriteLine("TFS Changeset #" + newChangesetId + " was created");
+                parentChangeset.Remote.Fetch();
+
+                var repo = parentChangeset.Remote.Repository;
+                // if committed changeset is 'ultimate parent' to the HEAD, i.e. every parent of refToCheckin is also
+                // parent of HEAD - then we could just rebase HEAD to newly created changeset safely
+                string revList = null;
+                repo.CommandOutputPipe(output => revList = output.ReadToEnd(), "rev-list", "^HEAD", refToCheckin);
+                bool ultimateParent = revList == "";
+                if (!ultimateParent)
+                    _stdout.WriteLine("Can't rebase HEAD to changeset safely as it has parents not from the changeset");
+                else
+                {
+                    _stdout.WriteLine("Rebasing onto committed changeset...");
+                    parentChangeset.Remote.Repository.CommandNoisy("rebase", parentChangeset.Remote.MaxCommitHash);
+                }
+                return GitTfsExitCodes.OK;
+            }
+            else
+            {
+                var newChangesetId = DoCheckin(parentChangeset, refToCheckin);
+                _stdout.WriteLine("TFS Changeset #" + newChangesetId + " was created. Marking it as a merge commit...");
+                parentChangeset.Remote.Fetch(new Dictionary<long, string> { { newChangesetId, refToCheckin } });
+                if (refToCheckin == "HEAD")
+                    parentChangeset.Remote.Repository.CommandNoisy("merge", parentChangeset.Remote.MaxCommitHash);
+                return GitTfsExitCodes.OK;
+            }
         }
 
         protected abstract long DoCheckin(TfsChangesetInfo changeset, string refToCheckin);
