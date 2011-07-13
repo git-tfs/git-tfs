ac30a2542fb82f95088749db642c094b07681dbb
ivan-danilov <van.danilov@gmail.com>
2011-07-13 23:06:32 +0300

    Minor changes according to review.

--- GitTfs/Commands/Rcheckin.cs
+++ GitTfs/Commands/Rcheckin.cs
@@ -54,19 +54,17 @@ namespace Sep.Git.Tfs.Commands
                     .WithRecommendation("Try to rebase HEAD onto latest TFS checkin and repeat rcheckin or alternatively checkin s");
             }
 
-            string revList = null;
             string tfsLatest = parentChangeset.Remote.MaxCommitHash;
 
-            // we could rcheckin only if tfsLatest changeset is a parent of refToRCheckin
+            // we could rcheckin only if tfsLatest changeset is a parent of HEAD
             // so that we could rebase after each single checkin without conflicts
-            repo.CommandOutputPipe(output => revList = output.ReadToEnd(), "rev-list", tfsLatest, "^HEAD");
-            if (revList != "")
+            if (repo.CommandOneline("rev-list", tfsLatest, "^HEAD") != "")
                 throw new GitTfsException("error: latest TFS commit should be parent of commits being checked in");
 
             while (true)
             {
                 // determine first descendant of tfsLatest
-                repo.CommandOutputPipe(output => revList = output.ReadLine(), "rev-list", "--abbrev-commit", "--parents", "--ancestry-path", "--first-parent", "--reverse", tfsLatest + "..HEAD");
+                string revList = repo.CommandOneline("rev-list", "--abbrev-commit", "--parents", "--ancestry-path", "--first-parent", "--reverse", tfsLatest + "..HEAD");
                 if (String.IsNullOrEmpty(revList))
                 {
                     _stdout.WriteLine("No more to rcheckin.");
