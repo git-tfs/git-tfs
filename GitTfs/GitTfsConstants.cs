fe9f4d36003c84b30e935690331775b932d82a20
Christopher Broome <ChrisBroome7@gmail.com>
2012-01-25 11:32:56 -0500

    Fix issue #123
    Update git log output to use a consistent format, despite any local
    changes made by the user.
    Update commit regex to correctly parse the new log format.
    Update commit regex to use a named capture.

--- GitTfs/GitTfsConstants.cs
+++ GitTfs/GitTfsConstants.cs
@@ -6,7 +6,7 @@ namespace Sep.Git.Tfs
     {
         public static readonly Regex Sha1 = new Regex("[a-f\\d]{40}", RegexOptions.IgnoreCase);
         public static readonly Regex Sha1Short = new Regex("[a-f\\d]{4,40}", RegexOptions.IgnoreCase);
-        public static readonly Regex CommitRegex = new Regex("^commit (" + Sha1 + ")\\s*$");
+        public static readonly Regex CommitRegex = new Regex(string.Concat(@"^commit (?<commit>", Sha1, @")"), RegexOptions.Compiled);
 
         public const string DefaultRepositoryId = "default";
 
