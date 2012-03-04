4aaacb57d5070f09bf2768af0056bb4456474f2c
Sean M. Collins <sean@coreitpro.com>
2012-03-04 14:50:41 -0500

    Add VS11 files
    TODO: fix deprecation warnings - use a TfsTeamProjectCollection
    constructor that utilizes a TfsClientCredentials object

---
+++ GitTfs.Vs11/TfsHelper.Vs11.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using Microsoft.Win32;
+using Sep.Git.Tfs.Core;
+using Sep.Git.Tfs.Core.TfsInterop;
+using Sep.Git.Tfs.Util;
+using Sep.Git.Tfs.VsCommon;
+using StructureMap;
+
+namespace Sep.Git.Tfs.Vs11
+{
+    using System.Net;
+
+    using Microsoft.TeamFoundation.Framework.Client;
+
+    public class TfsHelper : TfsHelperBase
+    {
+        private readonly TfsApiBridge _bridge;
+        private TfsTeamProjectCollection _server;
+
+        public TfsHelper(TextWriter stdout, TfsApiBridge bridge, IContainer container) : base(stdout, bridge, container)
+        {
+            _bridge = bridge;
+        }
+
+        public override string TfsClientLibraryVersion
+        {
+            get { return typeof(TfsTeamProjectCollection).Assembly.GetName().Version + " (MS)"; }
+        }
+
+        public override void EnsureAuthenticated()
+        {
+            if (string.IsNullOrEmpty(Url))
+            {
+                _server = null;
+            }
+            else
+            {
+                Uri uri;
+                if (!Uri.IsWellFormedUriString(Url, UriKind.Absolute))
+                {
+                    // maybe it is not an Uri but instance name
+                    var servers = RegisteredTfsConnections.GetConfigurationServers();
+                    var registered = servers.FirstOrDefault(s => String.Compare(s.Name, Url, StringComparison.OrdinalIgnoreCase) == 0);
+                    if (registered == null)
+                        throw new GitTfsException("Given tfs name is not correct URI and not found as a registered TFS instance");
+                    uri = registered.Uri;
+                }
+                else
+                {
+                    uri = new Uri(Url);
+                }
+
+                // TODO: Use TfsTeamProjectCollection constructor that takes a TfsClientCredentials object
+                _server = HasCredentials ?
+                    new TfsTeamProjectCollection(uri, GetCredential(), new UICredentialsProvider()) :
+                    new TfsTeamProjectCollection(uri, new UICredentialsProvider());
+
+                _server.EnsureAuthenticated();
+            }
+        }
+
+        protected override T GetService<T>()
+        {
+            return (T) _server.GetService(typeof (T));
+        }
+
+        protected override string GetAuthenticatedUser()
+        {
+            return VersionControl.AuthorizedUser;
+        }
+
+        public override bool CanShowCheckinDialog { get { return true; } }
+
+        public override long ShowCheckinDialog(IWorkspace workspace, IPendingChange[] pendingChanges, IEnumerable<IWorkItemCheckedInfo> checkedInfos, string checkinComment)
+        {
+            return ShowCheckinDialog(_bridge.Unwrap<Workspace>(workspace),
+                                     pendingChanges.Select(p => _bridge.Unwrap<PendingChange>(p)).ToArray(),
+                                     checkedInfos.Select(c => _bridge.Unwrap<WorkItemCheckedInfo>(c)).ToArray(),
+                                     checkinComment);
+        }
+
+        private long ShowCheckinDialog(Workspace workspace, PendingChange[] pendingChanges, 
+            WorkItemCheckedInfo[] checkedInfos, string checkinComment)
+        {
+            using (var parentForm = new ParentForm())
+            {
+                parentForm.Show();
+
+                var dialog = Activator.CreateInstance(GetCheckinDialogType(), new object[] {workspace.VersionControlServer});
+
+                return dialog.Call<int>("Show", parentForm.Handle, workspace, pendingChanges, pendingChanges,
+                                        checkinComment, null, null, checkedInfos);
+            }
+        }
+
+        private const string DialogAssemblyName = "Microsoft.TeamFoundation.VersionControl.ControlAdapter";
+
+        private static Type GetCheckinDialogType()
+        {
+            return GetDialogAssembly().GetType(DialogAssemblyName + ".CheckinDialog");
+        }
+
+        private static Assembly GetDialogAssembly()
+        {
+            return Assembly.LoadFrom(GetDialogAssemblyPath());
+        }
+
+        private static string GetDialogAssemblyPath()
+        {
+            return Path.Combine(GetVs2010InstallDir(), "PrivateAssemblies", DialogAssemblyName + ".dll");
+        }
+
+        private static string GetVs2010InstallDir()
+        {
+            return TryGetRegString(@"Software\Microsoft\VisualStudio\10.0", "InstallDir")
+                ?? TryGetRegString(@"Software\WOW6432Node\Microsoft\VisualStudio\10.0", "InstallDir");
+        }
+
+        private static string TryGetRegString(string path, string name)
+        {
+            try
+            {
+                Trace.WriteLine("Trying to get " + path + "|" + name);
+                var key = Registry.LocalMachine.OpenSubKey(path);
+                if(key != null)
+                {
+                    return key.GetValue(name) as string;
+                }
+            }
+            catch(Exception e)
+            {
+                Trace.WriteLine("Unable to get registry value " + path + "|" + name + ": " + e);
+            }
+            return null;
+        }
+    }
+
+    public class ItemDownloadStrategy : IItemDownloadStrategy
+    {
+        private readonly TfsApiBridge _bridge;
+
+        public ItemDownloadStrategy(TfsApiBridge bridge)
+        {
+            _bridge = bridge;
+        }
+
+        public Stream DownloadFile(IItem item)
+        {
+            try
+            {
+                return _bridge.Unwrap<Item>(item).DownloadFile();
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(String.Format("Something went wrong downloading \"{0}\" in changeset {1}", item.ServerItem, item.ChangesetId));
+                throw;
+            }
+        }
+    }
+}
