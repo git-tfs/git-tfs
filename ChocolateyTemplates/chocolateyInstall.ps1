f5c815a6b756c5d014e3364ac7bc3fd891835bb1
Matt Burke <spraints@gmail.com>
2013-04-01 11:48:56 -0400

    Push the chocolatey package.

--- ChocolateyTemplates/chocolateyInstall.ps1
+++ ChocolateyTemplates/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ try {
   $sysDrive = $env:SystemDrive
   $gittfsPath = "$sysDrive\tools\gittfs"
   
-  Install-ChocolateyZipPackage 'gittfs' '{{DownloadUrl}}' $gittfsPath
+  Install-ChocolateyZipPackage 'gittfs' '${DownloadUrl}' $gittfsPath
   Install-ChocolateyPath $gittfsPath
 
   write-host 'git-tfs has been installed. Call git tfs from the command line to see options. You may need to close and reopen the command shell.'
