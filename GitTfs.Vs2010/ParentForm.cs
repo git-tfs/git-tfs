c7e5f7cf1af0ee8af86a13909723a40a49a4c49f
Philippe Miossec <pmiossec@gmail.com>
2013-05-05 17:18:48 +0200

    Show CheckinTool window in task bar

--- GitTfs.Vs2010/ParentForm.cs
+++ GitTfs.Vs2010/ParentForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,17 +12,20 @@ namespace Sep.Git.Tfs.Vs2010
         public ParentForm()
         {
             WindowState = FormWindowState.Minimized;
-            ShowInTaskbar = false;
+            ShowInTaskbar = true;
+            ShowIcon = false;
             FormBorderStyle = FormBorderStyle.None;
             StartPosition = FormStartPosition.CenterParent;
+
         }
 
         protected override void OnLoad(EventArgs e)
         {
-            Visible = false;
             base.OnLoad(e);
             WindowState = FormWindowState.Normal;
             CenterToParent();
+            Text = "Checkin tool (git-tfs)";
+            Size = new Size(0, 0);
         }
     }
 }
