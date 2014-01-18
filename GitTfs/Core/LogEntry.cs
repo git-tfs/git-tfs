334cc7f87cf6d6dfb27d0ad2d8cb57f7dbf0cc59
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2014-01-19 01:00:06 +0400

    LogEntry members reordered

--- GitTfs/Core/LogEntry.cs
+++ GitTfs/Core/LogEntry.cs
@@ -12,22 +12,17 @@ namespace Sep.Git.Tfs.Core
         }
 
         public string AuthorName { get; set; }
-        public Tree Tree { get; set; }
+        public string AuthorEmail { get; set; }
+        public string CommitterName { get; set; }
+        public string CommitterEmail { get; set; }
 
+        public DateTime Date { get; set; }
+        public string Log { get; set; }
+
+        public Tree Tree { get; set; }
         public IList<string> CommitParents { get; private set; }
 
         public long ChangesetId { get; set; }
-
-        public string Log { get; set; }
-
-        public string AuthorEmail { get; set; }
-
-        public DateTime Date { get; set; }
-
-        public string CommitterName { get; set; }
-
-        public string CommitterEmail { get; set; }
-
         public IGitTfsRemote Remote { get; set; }
     }
 }
