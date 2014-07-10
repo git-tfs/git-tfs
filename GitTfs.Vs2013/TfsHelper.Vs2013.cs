e951a96afed00d2f15b6cdf53a20ac4d620ca398
Philippe Miossec <pmiossec@gmail.com>
2014-07-10 23:48:15 +0200

    Simplify a little more registry read for VS>=2012

--- GitTfs.Vs2013/TfsHelper.Vs2013.cs
+++ GitTfs.Vs2013/TfsHelper.Vs2013.cs
@@ -6,21 +6,9 @@ namespace Sep.Git.Tfs.Vs2013
 {
     public class TfsHelper : TfsHelperVs2012Base
     {
-        public TfsHelper(TextWriter stdout, TfsApiBridge bridge, IContainer container) : base(stdout, bridge, container)
-        {
-        }
+        protected override string TfsVersionString { get { return "12.0"; } }
 
-        private string vsInstallDir;
-        protected override string GetVsInstallDir()
-        {
-            if (vsInstallDir == null)
-            {
-                vsInstallDir = TryGetRegString(@"Software\Microsoft\VisualStudio\12.0", "InstallDir")
-                    ?? TryGetRegString(@"Software\WOW6432Node\Microsoft\VisualStudio\12.0", "InstallDir")
-                    ?? TryGetUserRegString(@"Software\Microsoft\WDExpress\12.0_Config", "InstallDir")
-                    ?? TryGetUserRegString(@"Software\WOW6432Node\Microsoft\WDExpress\12.0_Config", "InstallDir");
-            }
-            return vsInstallDir;
-        }
+        public TfsHelper(TextWriter stdout, TfsApiBridge bridge, IContainer container)
+            : base(stdout, bridge, container) { }
     }
 }
--- GitTfs.VsCommon/TfsHelper.Vs2012Base.cs
+++ GitTfs.VsCommon/TfsHelper.Vs2012Base.cs
@@ -1,18 +1,32 @@
 using System.IO;
 using Microsoft.TeamFoundation.VersionControl.Client;
+using Sep.Git.Tfs.Core;
 using StructureMap;
 
 namespace Sep.Git.Tfs.VsCommon
 {
     public abstract class TfsHelperVs2012Base : TfsHelperBase
     {
-        protected TfsHelperVs2012Base(TextWriter stdout, TfsApiBridge bridge, IContainer container) : base(stdout, bridge, container)
-        {
-        }
+        protected abstract string TfsVersionString { get; }
+        protected TfsHelperVs2012Base(TextWriter stdout, TfsApiBridge bridge, IContainer container)
+            : base(stdout, bridge, container) { }
 
         protected override bool HasWorkItems(Changeset changeset)
         {
             return Retry.Do(() => changeset.AssociatedWorkItems.Length > 0);
+        }
+
+        private string vsInstallDir;
+        protected override string GetVsInstallDir()
+        {
+            if (vsInstallDir == null)
+            {
+                vsInstallDir = TryGetRegString(@"Software\WOW6432Node\Microsoft\VisualStudio\" + TfsVersionString, "InstallDir")
+                    ?? TryGetRegString(@"Software\Microsoft\VisualStudio\" + TfsVersionString, "InstallDir")
+                    ?? TryGetUserRegString(@"Software\WOW6432Node\Microsoft\WDExpress\" + TfsVersionString + "_Config", "InstallDir")
+                    ?? TryGetUserRegString(@"Software\Microsoft\WDExpress\" + TfsVersionString + "_Config", "InstallDir");
+            }
+            return vsInstallDir;
         }
     }
 }
