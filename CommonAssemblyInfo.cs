ee5c0b3aa70c5c210210033f19338292575fdb6b
Philippe Miossec <pmiossec@gmail.com>
2015-07-18 23:54:51 +0200

    Update Copyright

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyCompany("SEP")]
 [assembly: AssemblyProduct("GitTfs")]
-[assembly: AssemblyCopyright("Copyright © 2009-2010")]
+[assembly: AssemblyCopyright("Copyright © 2009-2015")]
 [assembly: AssemblyVersion(GitTfsProperties.Version)]
 [assembly: AssemblyFileVersion(GitTfsProperties.Version)]
