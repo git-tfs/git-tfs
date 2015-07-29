8c97d8fc90bef2dd0046dbe1eb63d66b383c72f7
Michael Naumov <mnaoumov@gmail.com>
2015-07-29 23:57:36 +0300

    Override GetDialogAssemblyPath for TFS2015

--- GitTfs.Vs2015/TfsHelper.Vs2015.cs
+++ GitTfs.Vs2015/TfsHelper.Vs2015.cs
@@ -10,5 +10,11 @@ namespace Sep.Git.Tfs.Vs2015
 
         public TfsHelper(TextWriter stdout, TfsApiBridge bridge, IContainer container)
             : base(stdout, bridge, container) { }
+
+        protected override string GetDialogAssemblyPath()
+        {
+            var tfsExtensionsFolder = TryGetUserRegString(@"Software\Microsoft\VisualStudio\14.0\ExtensionManager\EnabledExtensions", "Microsoft.VisualStudio.TeamFoundation.TeamExplorer.Extensions,14.0.23102");
+            return Path.Combine(tfsExtensionsFolder, DialogAssemblyName + ".dll");
+        }
     }
 }
