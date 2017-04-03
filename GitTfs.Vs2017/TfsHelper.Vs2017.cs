4f57c9bc84109d5fdb8c4c4c9a9ccac48ab38736
Craig E. Shea <craig.e.shea@gmail.com>
2017-04-03 08:35:16 -0400

    Add support for VS 2017
    This commit links in the common files that the other VS* projects link
    to, and also adds specific support for Visual Studio 2017. The only file
    that is not linked in is the TfsHelper.Vs2012Base.cs file. This is
    because Visual Studio 2017 is drastically different than Visual Studio
    2012-2015 in terms of its installation and how you find information
    about installed Visual Studio instances. Therefore, TfsHelper.Vs2017.cs
    inherits directly from TfsHelperBase directly, and overrides the
    required methods.
    
    Visual Studio 2017 does not install many things into the Windows
    Registry. There is guidance online that details how to determine the
    installation path of an instance of Visual Studio 2017. (There can be
    many instances of Visual Studio 2017 installed on any one machine now.)
    This guidance has been used to determine the installation path, which in
    turn is used to find the DLL containing the TFS Checkin Form.
    
    The TFS Checkin Form is still in a DLL which is part of a Visual Studio
    extension, but the extension now has a deterministic installation
    location within the Visual Studio 2017 installation path.
    
    Signed-off-by: Craig E. Shea <craig.e.shea@gmail.com>

---
+++ GitTfs.Vs2017/TfsHelper.Vs2017.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.TeamFoundation.Build.Client;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Server;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.VisualStudio.Setup.Configuration;
+using Sep.Git.Tfs.Core.TfsInterop;
+using Sep.Git.Tfs.VsCommon;
+using StructureMap;
+
+namespace Sep.Git.Tfs.Vs2017
+{
+    public class TfsHelper : TfsHelperBase
+    {
+        private const int REGDB_E_CLASSNOTREG = unchecked((int)0x80040154);
+
+        private const string privateAssembliesFolder =
+            @"Common7\IDE\PrivateAssemblies";
+
+        private const string tfsExtensionsFolder =
+            @"Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer";
+
+        protected string TfsVersionString { get { return "15.0"; } }
+
+        public TfsHelper(TfsApiBridge bridge, IContainer container)
+            : base(bridge, container)
+        { }
+
+        protected override bool HasWorkItems(Changeset changeset)
+        {
+            return Retry.Do(() => changeset.AssociatedWorkItems.Length > 0);
+        }
+
+        protected override string GetVsInstallDir()
+        {
+            try
+            {
+                string visualStudioInstallationPath = null;
+
+                var setupConfiguration = (ISetupConfiguration2)GetSetupConfiguration();
+                var instancesEnumerator = setupConfiguration.EnumAllInstances();
+
+                int fetched;
+                var instances = new ISetupInstance[1];
+                do
+                {
+                    instancesEnumerator.Next(1, instances, out fetched);
+                    if (fetched > 0)
+                    {
+                        var instance = (ISetupInstance2)instances[0];
+                        var state = instance.GetState();
+                        if ((state & InstanceState.Local) == InstanceState.Local)
+                        {
+                            visualStudioInstallationPath = instance.GetInstallationPath();
+                        }
+                    }
+                }
+                while (fetched > 0 && !string.IsNullOrEmpty(visualStudioInstallationPath));
+
+                return visualStudioInstallationPath;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override IBuildDetail GetSpecificBuildFromQueuedBuild(IQueuedBuild queuedBuild, string shelvesetName)
+        {
+            var build = queuedBuild.Builds.FirstOrDefault(b => b.ShelvesetName == shelvesetName);
+            return build != null ? build : queuedBuild.Build;
+        }
+
+#pragma warning disable 618
+        private IGroupSecurityService GroupSecurityService
+        {
+            get { return GetService<IGroupSecurityService>(); }
+        }
+
+        public override IIdentity GetIdentity(string username)
+        {
+            return _bridge.Wrap<WrapperForIdentity, Identity>(Retry.Do(() => GroupSecurityService.ReadIdentity(SearchFactor.AccountName, username, QueryMembership.None)));
+        }
+
+        protected override TfsTeamProjectCollection GetTfsCredential(Uri uri)
+        {
+            return HasCredentials ?
+                new TfsTeamProjectCollection(uri, new TfsClientCredentials(new WindowsCredential(GetCredential()))) :
+                TfsTeamProjectCollectionFactory.GetTeamProjectCollection(uri);
+#pragma warning restore 618
+        }
+
+
+        protected override string GetDialogAssemblyPath()
+        {
+            return Path.Combine(GetVsInstallDir(), tfsExtensionsFolder, DialogAssemblyName + ".dll");
+        }
+
+        protected override Assembly LoadFromVsFolder(object sender, ResolveEventArgs args)
+        {
+            Trace.WriteLine("Looking for assembly " + args.Name + " ...");
+            string folderPath = Path.Combine(GetVsInstallDir(), privateAssembliesFolder);
+
+            string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
+            if (File.Exists(assemblyPath))
+            {
+                Trace.WriteLine("... loading " + args.Name + " from " + assemblyPath);
+                return Assembly.LoadFrom(assemblyPath);
+            }
+
+            return null;
+        }
+
+        private static ISetupConfiguration GetSetupConfiguration()
+        {
+            try
+            {
+                // Try to CoCreate the class object.
+                return new SetupConfiguration();
+            }
+            catch (COMException ex)
+            {
+                if (ex.HResult == REGDB_E_CLASSNOTREG)
+                {
+                    // Attempt to get the class object using an app-local call.
+                    ISetupConfiguration setupConfiguration;
+
+                    var result = GetSetupConfiguration(out setupConfiguration, IntPtr.Zero);
+                    if (result < 0)
+                    {
+                        throw new COMException("Failed to get setup configuration query.", result);
+                    }
+
+                    return setupConfiguration;
+                }
+
+                throw ex;
+            }
+        }
+
+        [DllImport("Microsoft.VisualStudio.Setup.Configuration.Native.dll", ExactSpelling = true, PreserveSig = true)]
+        static extern int GetSetupConfiguration([MarshalAs(UnmanagedType.Interface), Out] out ISetupConfiguration configuration, IntPtr reserved);
+    }
+}
