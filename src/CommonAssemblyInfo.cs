f3126b92ef83ecf6da7f37874320ad364666fbf9
Philippe Miossec <pmiossec@gmail.com>
2018-11-15 14:20:13 +0100

    Convert csproj to new format + adaptations

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCompany("SEP")]
+[assembly: AssemblyCompany("GitTfs")]
 [assembly: AssemblyProduct("GitTfs")]
-[assembly: AssemblyCopyright("Copyright © 2009-2016")]
+[assembly: AssemblyCopyright("Copyright © 2009-2019")]
 [assembly: AssemblyVersion("0.0.1")]
 [assembly: AssemblyFileVersion("0.0.1")]
 [assembly: AssemblyInformationalVersion("0.0.1+f2db4336ba.shouldBeGeneratedByCakeDuringBuild")]
