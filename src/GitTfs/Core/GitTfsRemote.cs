37feaa3364bba0510a5e4c8f97d757f77107ef46
Philippe Miossec <pmiossec@gmail.com>
2019-01-25 17:34:01 +0100

    Display a warning when a file is ignored (#1213)

--- src/GitTfs/Core/GitTfsRemote.cs
+++ src/GitTfs/Core/GitTfsRemote.cs
@@ -253,7 +253,13 @@ namespace GitTfs.Core
 
         private bool IsIgnored(string path)
         {
-            return Ignorance.IsIncluded(path) || Repository.IsPathIgnored(path);
+            var isIgnored = Ignorance.IsIncluded(path) || Repository.IsPathIgnored(path);
+            if (isIgnored)
+            {
+                Trace.TraceWarning("The file '{0}' is ignored due to `.gitignore` or ignore regex defined.", path);
+            }
+
+            return isIgnored;
         }
 
         private Bouncer _ignorance;
