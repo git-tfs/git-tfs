67536ca9bbfc6be46125cedbb6774f77f3449699
nikita-r <Nikita.Retyunskiy@moodys.com>
2019-09-10 17:07:44 -0400

    fixed: prefix check for ServerItem path

--- src/GitTfs.VsCommon/TfsHelper.Common.cs
+++ src/GitTfs.VsCommon/TfsHelper.Common.cs
@@ -219,7 +219,7 @@ namespace GitTfs.VsCommon
 
             var changes = VersionControl.GetChangesForChangeset(targetChangeset, false, Int32.MaxValue, null, null, true).Where(change =>
             {
-                return change.ChangeType.HasFlag(ChangeType.Merge) && change.Item.ServerItem.Contains(path);
+                return change.ChangeType.HasFlag(ChangeType.Merge) && change.Item.ServerItem.StartsWith(path, StringComparison.OrdinalIgnoreCase);
             });
             if (changes.Empty())
                 return -1;
